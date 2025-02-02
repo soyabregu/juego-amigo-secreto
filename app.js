14d2e82985d789012f4e81514d96efeed2c09177
m.soledad abregu <soyabregu247@gmail.com>
2025-02-02 15:23:20 -0300

    creandoArrayDeAlmacenamiento

--- app.js
+++ app.js
@@ -1,2 +1,1 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let 
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problemafunction
